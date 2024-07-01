a30a733d50f64ceed6b6e99ffd91761e3abb44e9
Charles Knell <charles1283@gmail.com>
2024-07-01 11:58:37 -0500

    In consolidate-excel-worksheets.py, added creation of the output folder if it doesn't exist. Also, updated comments to include the step for installing openpyxl. In README.md, updated usage steps.

--- consolidate-excel-worksheets.py
+++ consolidate-excel-worksheets.py
@@ -2,23 +2,29 @@
 
 # tag=charlesknell Version 2
 # Credits: article by Gaurav Siyal https://www.makeuseof.com/consolidate-excel-workbooks-python/
-# Updated for Python 3.12.3, added usage instructions, a few helpful changes, and updated comments
+# 30 Jun 24 - Updated for Python 3.12.3, added usage instructions, a few helpful changes, and updated comments
+# 01 Jul 24 - Create the output folder if it doesn't exist
 
 # Description: This python program will consolidate/merge a large number of .xlsx Excel files, limited
 #              only by your computer's memory.
 
-# Usage:  1. Python must be installed. See https://python.org
-#         2. Pandas must be installed. Install at a Windows Command Prompt using "pip install pandas"
-#         3. Copy this file into the folder containing the .xlsx files to be consolidated/merged.
-#         4. Edit the input_file_path and output_file_path to meet your needs. The output_file_path should be
+# Usage:  1. python must be installed. See https://python.org
+#         2. pandas must be installed. Install at a Windows Command Prompt using "pip install pandas"
+#         3. openpyxl must be installed. Install at a Windows Command Prompt using "pip install openpyxl"
+#         4. Copy this file into the folder containing the .xlsx files to be consolidated/merged.
+#         5. Edit the input_file_path and output_file_path to meet your needs. The output_file_path should be
 #            different compared to the input_file_path, for repeatable results.
-#         5. Execute this program at a Windows Command Prompt using "python consolidate-excel-worksheets.py"
+#         6. Execute this program at a Windows Command Prompt using "python consolidate-excel-worksheets.py"
 
 import pandas as pd
 import os
 
 input_file_path = "./"  # currently set to the current working folder
 output_file_path = "./output/"  # currently set to a folder in the current working folder named output
+
+# create the output folder if it doesn't exist
+if not os.path.exists(output_file_path):
+    os.makedirs(output_file_path)
 
 # create a list of all the file references of the input folder
 file_list = os.listdir(input_file_path)
