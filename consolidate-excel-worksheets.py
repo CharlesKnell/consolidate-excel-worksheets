5f49885a2df7f9e86d42c93dbfc053d76b99709e
Charles Knell <charles1283@gmail.com>
2024-06-30 19:35:34 -0500

    added the Usage section in the comments of consolidate-excel-worksheets.py

--- consolidate-excel-worksheets.py
+++ consolidate-excel-worksheets.py
@@ -4,13 +4,15 @@
 # Credits: article by Gaurav Siyal https://www.makeuseof.com/consolidate-excel-workbooks-python/
 # Updated for Python 3.12.3, added usage instructions, a few helpful changes, and updated comments
 
-# Usage: Copy this file into the folder containing the .xlsx files to be consolidated or merged.
-#        Edit the input_file_path and output_file_path to meet your needs.
-#        The output_file_path should be different compared to the input_file_path, for repeatable results.
-#
-#        1. Python must be installed. See https://python.org
-#        2. Pandas must be installed. Install at a Command Prompt using "pip install pandas"
-#        3. Execute this program at a Command Prompt using "python consolidate-excel-worksheets.py"
+# Description: This python program will consolidate/merge a large number of .xlsx Excel files, limited
+#              only by your computer's memory.
+
+# Usage:  1. Python must be installed. See https://python.org
+#         2. Pandas must be installed. Install at a Windows Command Prompt using "pip install pandas"
+#         3. Copy this file into the folder containing the .xlsx files to be consolidated/merged.
+#         4. Edit the input_file_path and output_file_path to meet your needs. The output_file_path should be
+#            different compared to the input_file_path, for repeatable results.
+#         5. Execute this program at a Windows Command Prompt using "python consolidate-excel-worksheets.py"
 
 
 import pandas as pd
